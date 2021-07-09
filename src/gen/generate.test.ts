54e5cda1c6a382b3c4ee75c0db46ec32f78c02b6
Michael Tiller <michael.tiller@gmail.com>
2021-07-09 14:27:11 -0400

    I was setting options incorrectly
    The regressions here are actually picking up the result of fixing the bug.

--- src/gen/generate.test.ts
+++ src/gen/generate.test.ts
@@ -5,14 +5,14 @@ import { loadSpec } from "../specification";
 import { generate } from "./generate";
 
 describe("Test sample AST specifications", () => {
-  ["expr", "repmin", "kitchen"].forEach((file) => {
+  ["expr", "repmin", "kitchen", "dogfood"].forEach((file) => {
     it(`should generate proper output for ${file}.yaml using json`, () => {
       const raw = fs.readFileSync(
         path.join(__dirname, "..", "..", "samples", `${file}.yaml`)
       );
       const spec = yaml.parse(raw.toString());
-      spec.options.optional = "json";
       const types = loadSpec(spec);
+      types.options.optional = "json";
       const output = generate(types);
 
       expect(types).toMatchSnapshot();
@@ -23,8 +23,8 @@ describe("Test sample AST specifications", () => {
         path.join(__dirname, "..", "..", "samples", `${file}.yaml`)
       );
       const spec = yaml.parse(raw.toString());
-      spec.options.optional = "purify";
       const types = loadSpec(spec);
+      types.options.optional = "purify";
       const output = generate(types);
 
       expect(types).toMatchSnapshot();
