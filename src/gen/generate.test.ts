2f05af211cfd039c119ddb33a39977cd6669747b
Michael Tiller <michael.tiller@gmail.com>
2021-07-09 09:22:32 -0400

    Added snapshot tests

---
+++ src/gen/generate.test.ts
@@ -0,0 +1,20 @@
+import fs from "fs";
+import path from "path";
+import yaml from "yaml";
+import { loadSpec } from "../specification";
+import { generate } from "./generate";
+
+describe("Test sample AST specifications", () => {
+  ["expr"].forEach((file) => {
+    it(`should generate proper output for ${file}.yaml`, () => {
+      const raw = fs.readFileSync(
+        path.join(__dirname, "..", "..", "samples", `${file}.yaml`)
+      );
+      const spec = yaml.parse(raw.toString());
+      const types = loadSpec(spec);
+      const output = generate(types);
+
+      expect(output).toMatchSnapshot();
+    });
+  });
+});
